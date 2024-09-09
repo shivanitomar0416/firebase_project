c079642d6f638ea5e922d7097f38e86c6234030b
shivanitomarofficial@gmail.com <shivanitomarofficial@gmail.com>
2024-09-09 22:57:20 +0530

    My_Code_11

--- lib/ui/posts/post_screen.dart
+++ lib/ui/posts/post_screen.dart
@@ -2,6 +2,7 @@ import 'package:firebase_project/ui/auth/signup_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_project/utils/utils.dart';
+import 'package:firebase_project/ui/auth/login_screen.dart';
 
 class PostScreen extends StatefulWidget {
   const PostScreen({super.key});
@@ -11,34 +12,32 @@ class PostScreen extends StatefulWidget {
 }
 
 class _PostScreenState extends State<PostScreen> {
-  final auth = FirebaseAuth.instance;
 
-  Future<void> _signOut() async {
-    try {
-      await auth.signOut();
-      Navigator.pushReplacement(
-        context,
-        MaterialPageRoute(builder: (context) => const SignUpScreen()),
-      );
-    } catch (error) {
-      Utils().toastMessage(error.toString());
-    }
-  }
+  final auth = FirebaseAuth.instance;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        
         title: const Text('Post'),
         actions: [
           IconButton(
-            onPressed: _signOut,
+            onPressed: (){
+              auth.signOut().then((value){
+                Navigator.push(context,
+                MaterialPageRoute(builder: (context) => loginScreen()));
+              }).onError((error, stackTrace){
+                    Utils().toastMessage(error.toString());
+
+              });
+            },
             icon: const Icon(Icons.logout), // Updated icon
           ),
-          const SizedBox(width: 10),
         ],
       ),
-       body: const Center(child: Text('Post Screen')), 
     );
   }
 }
+
+
